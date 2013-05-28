fca9f042fb7497594a470cf1d4a73af335b2ce1e
Daniel Geschwender <geschd@cs.ubc.ca>
2013-05-28 11:56:25 -0700

    RESOLVED - # 1664: Make HAL Startup Utility with JCommander https://mantis.sjrx.net/view.php?id=1664

---
+++ src/ca/ubc/cs/beta/hal/startup/JsonWriter.java
@@ -0,0 +1,97 @@
+package ca.ubc.cs.beta.hal.startup;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+
+import ca.ubc.cs.beta.aclib.configspace.ParamConfiguration;
+import ca.ubc.cs.beta.aclib.configspace.ParamConfigurationSpace;
+import ca.ubc.cs.beta.aclib.configspace.ParamConfiguration.StringFormat;
+import ca.ubc.cs.beta.aclib.execconfig.AlgorithmExecutionConfig;
+import ca.ubc.cs.beta.aclib.misc.jcommander.JCommanderHelper;
+import ca.ubc.cs.beta.aclib.misc.options.UsageSection;
+import ca.ubc.cs.beta.aclib.misc.returnvalues.ACLibReturnValues;
+import ca.ubc.cs.beta.aclib.misc.version.VersionTracker;
+import ca.ubc.cs.beta.aclib.options.AbstractOptions;
+import ca.ubc.cs.beta.aclib.options.ConfigToLaTeX;
+import ca.ubc.cs.beta.aclib.probleminstance.ProblemInstance;
+import ca.ubc.cs.beta.aclib.probleminstance.ProblemInstanceSeedPair;
+import ca.ubc.cs.beta.aclib.runconfig.RunConfig;
+import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.TargetAlgorithmEvaluator;
+import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.TargetAlgorithmEvaluatorBuilder;
+import ca.ubc.cs.beta.aclib.targetalgorithmevaluator.loader.TargetAlgorithmEvaluatorLoader;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+import ec.util.MersenneTwister;
+
+public class JsonWriter {
+
+	private static File json = new File("./hal.json");
+	private static String fileContent;
+	
+	public static void main(String[] args)
+	{
+		JsonOptions mainOptions = new JsonOptions();
+		JCommander com = new JCommander(mainOptions, true, true);
+		
+		try {	
+			//parse the JCommander options
+			com.parse(args);
+			
+			//If no database argument was provided
+			//and no arguments were provided at all and a JSON file exists, run with the current settings
+			//the database argument is required to write or overwrite the JSON file
+			if(mainOptions.database==null)
+			{
+				if(!json.exists())
+					throw new ParameterException("No JSON file found. Must supply argument --db to generate JSON file");	
+				else if(args.length!=0)
+					throw new ParameterException("Must supply argument --db in order to overwrite JSON file.");
+			}
+			//Prepare and write the JSON file
+			else
+			{
+				fileContent = "{\n  \"database\": \""+mainOptions.getJDBCString()+"\",\n  \"serverport\": "+mainOptions.serverport+",\n  \"reloadImportsOnStartup\": "+mainOptions.reload+"\n}";
+				
+				if(!json.exists())
+				{
+					json.createNewFile();
+				}
+				
+				FileWriter fwrite = new FileWriter(json.getAbsoluteFile());
+				BufferedWriter bwrite = new BufferedWriter(fwrite);
+				bwrite.write(fileContent);
+				bwrite.close();
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.exit(ACLibReturnValues.OTHER_EXCEPTION);
+		}
+		catch(IllegalStateException e){
+			e.printStackTrace();
+			System.exit(ACLibReturnValues.OTHER_EXCEPTION);
+		}
+		catch(ParameterException e){	
+			//Converts the actual option objects into objects "UsageSection"s that are easy to manipulate
+			List<UsageSection> sections = ConfigToLaTeX.getParameters(mainOptions);
+			
+			boolean showHiddenParameters = false;
+			
+			//A much nicer usage screen than JCommander's 
+			ConfigToLaTeX.usage(sections, showHiddenParameters);
+			e.printStackTrace();
+			System.exit(ACLibReturnValues.PARAMETER_EXCEPTION);
+		}
+		System.exit(ACLibReturnValues.SUCCESS);
+	}
+}
