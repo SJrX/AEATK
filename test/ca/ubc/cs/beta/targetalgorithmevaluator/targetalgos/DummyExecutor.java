6ff9f61ea35996a3ae28a61eb52c6083e170e534
Steve Ramage <seramage@cs.ubc.ca>
2013-07-30 16:58:45 -0700

    RESOLVED - # 1799: Change Log Message https://mantis.sjrx.net/view.php?id=1799

---
+++ test/ca/ubc/cs/beta/targetalgorithmevaluator/targetalgos/DummyExecutor.java
@@ -0,0 +1,22 @@
+package ca.ubc.cs.beta.targetalgorithmevaluator.targetalgos;
+
+import ca.ubc.cs.beta.TestHelper;
+import ca.ubc.cs.beta.aclib.configspace.ParamConfiguration;
+import ca.ubc.cs.beta.aclib.configspace.ParamConfigurationSpace;
+import ca.ubc.cs.beta.aclib.configspace.ParamConfiguration.StringFormat;
+
+public class DummyExecutor {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+	
+		System.out.println("Result for ParamILS: SAT, 1.000, 0, 0," + args[4] + "\n");
+	}
+
+	
+
+}
