15912b98868e77a42cc563f60ec46bc1c52ae120
Steve Ramage <seramage@cs.ubc.ca>
2014-04-09 10:05:48 -0700

    Resolved #2019 - Handle Multiple Trajectory Files at Once for Validation https://mantis.sjrx.net/view.php?id=2019

--- src/ca/ubc/cs/beta/aclib/trajectoryfile/TrajectoryFileOptions.java
+++ src/ca/ubc/cs/beta/aclib/trajectoryfile/TrajectoryFileOptions.java
@@ -3,7 +3,14 @@ package ca.ubc.cs.beta.aclib.trajectoryfile;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.beust.jcommander.Parameter;
 
@@ -14,16 +21,66 @@ import ca.ubc.cs.beta.aclib.options.AbstractOptions;
 @UsageTextField(hiddenSection = true)
 public class TrajectoryFileOptions extends AbstractOptions{
 
-	@Parameter(names={"--trajectory-file","--trajectoryFile"}, description="Trajectory File to read configurations from")
-	public File trajectoryFile;
+	@Parameter(names={"--trajectory-file","--trajectoryFile"}, description="Trajectory File to read configurations from", variableArity = true)
+	public List<String> trajectoryFiles = new ArrayList<String>();
+	
 	
 	@Parameter(names={"--trajectory-use-tunertime-if-no-walltime","--useTunerTimeIfNoWallTime"}, description="Use the tuner time as walltime if there is no walltime in the trajectory file")
 	public boolean useTunerTimeIfNoWallTime;
 
+	/**
+	 * 
+	 * @deprecated returns the first and only the first trajectory file specified
+	 */
 	public List<TrajectoryFileEntry> parseTrajectoryFile(ParamConfigurationSpace configSpace) throws FileNotFoundException, IOException {
-		return TrajectoryFileParser.parseTrajectoryFileAsList(trajectoryFile, configSpace, useTunerTimeIfNoWallTime);
+		
+		if(trajectoryFiles.size() > 0)
+		{
+			return TrajectoryFileParser.parseTrajectoryFileAsList(new File(trajectoryFiles.get(0)), configSpace, useTunerTimeIfNoWallTime);
+		} else
+		{
+			throw new IllegalArgumentException("No trajectory file specified (precondition of calling this");
+		}
 	}
 	
+	
+	private static final String TRAJ_RUN_REGEX = "traj-run-([0-9]+)\\.[ct][sx][vt]";
+	public Map<Integer, List<TrajectoryFileEntry>> parseTrajectoryFiles(ParamConfigurationSpace configSpace) throws FileNotFoundException, IOException {
+		
+		int newID=1000000;
+		
+		
+		Pattern p = Pattern.compile(TRAJ_RUN_REGEX);
+		
+		
+		Set<Integer> usedIntegers = new HashSet<Integer>();
+		Map<Integer, List<TrajectoryFileEntry>> tfes = new TreeMap<Integer,List<TrajectoryFileEntry>>();
+		//Tries to auto detect the number of the run from the file name, otherwise it starts at 1000000 and finds new runs
+		for(String trajFile : trajectoryFiles)
+		{
+			Matcher m = p.matcher((new File(trajFile)).getName());
+		
+			int id = 0;
+			if(m.find())
+			{
+				m.group(1);
+				
+				id = Integer.valueOf(m.group(1));
+			}
+			
+			
+			while(usedIntegers.contains(id))
+			{
+				id = newID++;
+			}
+			
+			usedIntegers.add(id);
+			
+			tfes.put(id,TrajectoryFileParser.parseTrajectoryFileAsList(new File(trajFile), configSpace, useTunerTimeIfNoWallTime));
+		}
+		
+		return tfes;
+	}
 	
 	
 }
