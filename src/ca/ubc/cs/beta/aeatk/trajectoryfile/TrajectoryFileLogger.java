098cc7a43f72d67aa2dd2190a478bc78e00aa4ae
Steve Ramage <seramage@cs.ubc.ca>
2014-08-13 23:24:07 -0700

    RESOLVED - #2062: Log Predicted Performance in Trajectory File https://mantis.sjrx.net/view.php?id=2062

--- src/ca/ubc/cs/beta/aeatk/trajectoryfile/TrajectoryFileLogger.java
+++ src/ca/ubc/cs/beta/aeatk/trajectoryfile/TrajectoryFileLogger.java
@@ -3,9 +3,13 @@ package ca.ubc.cs.beta.aeatk.trajectoryfile;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +18,14 @@ import ca.ubc.cs.beta.aeatk.eventsystem.EventHandler;
 import ca.ubc.cs.beta.aeatk.eventsystem.events.AutomaticConfiguratorEvent;
 import ca.ubc.cs.beta.aeatk.eventsystem.events.ac.AutomaticConfigurationEnd;
 import ca.ubc.cs.beta.aeatk.eventsystem.events.ac.IncumbentPerformanceChangeEvent;
+import ca.ubc.cs.beta.aeatk.eventsystem.events.model.ModelBuildEndEvent;
+import ca.ubc.cs.beta.aeatk.eventsystem.events.model.ModelBuildStartEvent;
 import ca.ubc.cs.beta.aeatk.misc.cputime.CPUTime;
 import ca.ubc.cs.beta.aeatk.parameterconfigurationspace.ParameterConfiguration;
 import ca.ubc.cs.beta.aeatk.parameterconfigurationspace.ParameterConfiguration.ParameterStringFormat;
 import ca.ubc.cs.beta.aeatk.runhistory.RunHistory;
 import ca.ubc.cs.beta.aeatk.termination.TerminationCondition;
+import ca.ubc.cs.beta.models.fastrf.RandomForest;
 
 public class TrajectoryFileLogger implements EventHandler<AutomaticConfiguratorEvent>{
 
@@ -40,6 +47,11 @@ public class TrajectoryFileLogger implements EventHandler<AutomaticConfiguratorE
 	
 	boolean closed = false;
 	
+	
+	private volatile SoftReference<Object> softModel = new SoftReference<Object>(null);
+	private volatile Object hardModel;
+	private AtomicBoolean modelPredictionErrorLogged = new AtomicBoolean(false);
+	
 	public TrajectoryFileLogger(RunHistory runHistory, TerminationCondition terminationCondition, String fileNamePrefix, ParameterConfiguration initialIncumbent, CPUTime cpuTime)
 	{
 		this.fileNamePrefix = fileNamePrefix;
@@ -55,7 +67,7 @@ public class TrajectoryFileLogger implements EventHandler<AutomaticConfiguratorE
 			trajectoryFileWriterCSV = new FileWriter(f.getParentFile().getAbsolutePath() + File.separator + "detailed-"+ f.getName() + ".csv");
 			
 			trajectoryFileWriter.append("\"CPU Time Used\",\"Estimated Training Performance\",\"Wallclock Time\",\"Incumbent ID\",\"Automatic Configurator (CPU) Time\",\"Configuration...\"\n");
-			trajectoryFileWriterCSV.append("\"CPU Time Used\",\"Estimated Training Performance\",\"Wallclock Time\",\"Incumbent ID\",\"Automatic Configurator (CPU) Time\",\"Full Configuration\"\n");
+			trajectoryFileWriterCSV.append("\"CPU Time Used\",\"Estimated Training Performance\",\"Wallclock Time\",\"Incumbent ID\",\"Automatic Configurator (CPU) Time\",\"Full Configuration\",\"Predicted Performance (if available)\",\n");
 			writeIncumbent(0,Double.MAX_VALUE,0,initialIncumbent,0);
 		} catch (IOException e) {
 			throw new IllegalStateException("Error occured creating files",e);
@@ -78,6 +90,7 @@ public class TrajectoryFileLogger implements EventHandler<AutomaticConfiguratorE
 		} else if(event instanceof AutomaticConfigurationEnd)
 		{
 			
+			hardModel = null;
 			log.debug("Writing trajectory file to {}",  (new File(fileNamePrefix)).getAbsolutePath());
 			
 			if(lastIevent != null)
@@ -93,6 +106,17 @@ public class TrajectoryFileLogger implements EventHandler<AutomaticConfiguratorE
 			}
 			
 			closed = true;
+		} else if(event instanceof ModelBuildStartEvent)
+		{
+				hardModel = null;
+				
+		}else if(event instanceof ModelBuildEndEvent)
+		{
+				Object theModel = ((ModelBuildEndEvent) event).getModelIfAvailable();
+				
+				
+				this.softModel = new SoftReference<Object>(theModel);
+				this.hardModel = theModel;
 		} else
 		{
 			log.error("Got an event I wasn't expecting: {}", event.getClass().getCanonicalName());
@@ -148,7 +172,50 @@ public class TrajectoryFileLogger implements EventHandler<AutomaticConfiguratorE
 		String escapedParamString = paramString.replaceAll(",","\\,");
 		
 		String outLine = tunerTime + ", " + empiricalPerformance + ", " + wallClockTime + ", " + thetaIdxInc + ", " + acTime + ", " + paramString +"\n";
-		String escapedOutLine = "\""+tunerTime + "\",\" " + empiricalPerformance + "\",\"" + wallClockTime + "\",\"" + thetaIdxInc + "\",\"" + acTime + "\", \"" + escapedParamString +"\"\n";
+		
+		double predictedPerformance = Double.NaN;
+
+		
+		Object theModel = hardModel;
+		
+		if(theModel == null)
+		{
+			theModel = this.softModel.get();
+		}
+		
+		
+		
+		
+		if(theModel != null)
+		{
+			if(theModel instanceof RandomForest)
+			{
+				 
+				RandomForest rf = (RandomForest) theModel;
+				
+				try 
+				{
+					predictedPerformance = this.applyMarginalModel(Collections.singletonList(incumbent),rf)[0][0];
+				} catch(RuntimeException e)
+				{
+					if(modelPredictionErrorLogged.compareAndSet(false, true))
+					{
+						log.error("Couldn't log predicted performance of model see error (possibly because model wasn't preprocessed marginal version (no further errors will be logged):",e);
+					}
+				}
+			} else
+			{
+				if(modelPredictionErrorLogged.compareAndSet(false, true))
+				{
+					log.warn("New Model Type Detected {}, not sure how to make predictions on it in ", theModel.getClass().getCanonicalName());
+				}
+			}
+		}
+		
+		String escapedOutLine = "\""+tunerTime + "\",\" " + empiricalPerformance + "\",\"" + wallClockTime + "\",\"" + thetaIdxInc + "\",\"" + acTime + "\", \"" + escapedParamString +"\",\""+predictedPerformance+"\"\n";
+		
+		
+		
 		log.trace("Logging incumbent: (Runs {}): {}", ((this.lastIevent != null) ? this.lastIevent.getIncumbentRunCount() : "?"), outLine.trim());
 		try 
 		{
@@ -169,5 +236,42 @@ public class TrajectoryFileLogger implements EventHandler<AutomaticConfiguratorE
 	}
 
 	
-
+	/**
+	 * Computes a marginal prediction across all instances for the configArrays.
+	 * @param configArrays
+	 * @return
+	 */
+	protected double[][] applyMarginalModel(double[][] configArrays, RandomForest forest)
+	{
+		//=== Use all trees.
+		int[] treeIdxsToUse = new int[forest.numTrees];
+		for(int i=0; i <  forest.numTrees; i++)
+		{
+			treeIdxsToUse[i]=i;
+		}
+		
+		return RandomForest.applyMarginal(forest,treeIdxsToUse,configArrays);
+		
+		
+	}
+	
+	/**
+	 * Computes a marginal prediction across all instances for the configs. 
+	 * @param configs
+	 * @return
+	 */
+	protected double[][] applyMarginalModel(List<ParameterConfiguration> configs, RandomForest forest)
+	{
+		//=== Translate into array format, and call method for that format.
+		double[][] configArrays = new double[configs.size()][];
+		int i=0; 
+		for(ParameterConfiguration config: configs)
+		{
+			configArrays[i] = config.toValueArray();
+			i++;
+		}
+	
+		return applyMarginalModel(configArrays, forest);		
+	}
+	
 }
