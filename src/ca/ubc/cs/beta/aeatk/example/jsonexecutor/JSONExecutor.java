fd6fa0f4beba575fb847ece10d643f116d86f5ce
Steve Ramage <seramage@cs.ubc.ca>
2015-08-15 18:28:00 -0700

    2120 Allow non-blocking usage of json-executor utility

--- src/ca/ubc/cs/beta/aeatk/example/jsonexecutor/JSONExecutor.java
+++ src/ca/ubc/cs/beta/aeatk/example/jsonexecutor/JSONExecutor.java
@@ -1,34 +1,14 @@
 package ca.ubc.cs.beta.aeatk.example.jsonexecutor;
 
-import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import ca.ubc.cs.beta.aeatk.algorithmexecutionconfiguration.AlgorithmExecutionConfiguration;
 import ca.ubc.cs.beta.aeatk.algorithmrunconfiguration.AlgorithmRunConfiguration;
 import ca.ubc.cs.beta.aeatk.algorithmrunresult.AlgorithmRunResult;
-import ca.ubc.cs.beta.aeatk.algorithmrunresult.RunStatus;
 import ca.ubc.cs.beta.aeatk.misc.jcommander.JCommanderHelper;
-import ca.ubc.cs.beta.aeatk.misc.version.VersionTracker;
+import ca.ubc.cs.beta.aeatk.misc.returnvalues.AEATKReturnValues;
 import ca.ubc.cs.beta.aeatk.options.AbstractOptions;
-import ca.ubc.cs.beta.aeatk.parameterconfigurationspace.ParameterConfiguration;
-import ca.ubc.cs.beta.aeatk.parameterconfigurationspace.ParameterConfigurationSpace;
-import ca.ubc.cs.beta.aeatk.parameterconfigurationspace.ParameterConfiguration.ParameterStringFormat;
-import ca.ubc.cs.beta.aeatk.probleminstance.ProblemInstance;
-import ca.ubc.cs.beta.aeatk.probleminstance.ProblemInstanceSeedPair;
 import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.TargetAlgorithmEvaluator;
-import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.TargetAlgorithmEvaluatorOptions;
+import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.TargetAlgorithmEvaluatorCallback;
 import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.TargetAlgorithmEvaluatorRunObserver;
-
+import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.WaitableTAECallback;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 import com.fasterxml.jackson.core.JsonFactory;
@@ -38,8 +18,15 @@ import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import ec.util.MersenneTwister;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * A simple utility class that provides the ability to execute a single run against a <code>TargetAlgorithmEvaluator</code>.
@@ -84,13 +71,8 @@ public class JSONExecutor
 				//So that options that take effect
 				log = LoggerFactory.getLogger(JSONExecutor.class);
 			}
-		
-	
-		
-			
-			
-			
-			List<AlgorithmRunResult> results = null;
+
+
 			try( TargetAlgorithmEvaluator tae = mainOptions.taeOptions.getTargetAlgorithmEvaluator( taeOptions) ) {
 				
 				log.info("Waiting for an array of AlgorithmRunConfiguration in JSON format to be recieved on STDIN. You can use the algo-test utility --print-json true to get example JSON.");
@@ -139,43 +121,78 @@ public class JSONExecutor
 					}
 					
 				};
-				
+
+				WaitableTAECallback taeCallback = new WaitableTAECallback(new TargetAlgorithmEvaluatorCallback(){
+
+					@Override
+					public void onSuccess(List<AlgorithmRunResult> runs) {
+						ObjectMapper map = new ObjectMapper();
+						JsonFactory factory = new JsonFactory();
+						factory.setCodec(map);
+
+
+						try {
+                            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+                            JsonGenerator g = factory.createGenerator(bout);
+
+                            SimpleModule sModule = new SimpleModule("MyModule", new Version(1, 0, 0, null));
+                            map.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+                            map.registerModule(sModule);
+
+                            List<AlgorithmRunResult> results2 = new ArrayList<>(runs);
+                            g.writeObject(results2);
+                            g.flush();
+
+                            System.out.flush();
+                            System.out.println("******JSON******\n" + bout.toString("UTF-8") + "\n********");
+                            System.out.flush();
+                        } catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+
+					}
+
+					@Override
+					public void onFailure(RuntimeException e) {
+                            e.printStackTrace();
+					}
+				});
+
+
 				if(mainOptions.printStatus)
 				{
 					log.info("Periodically printing status, use --print-status false to disable");
-					results = tae.evaluateRun(runsToDo, taeRunObserver);
+					tae.evaluateRunsAsync(runsToDo, taeCallback, taeRunObserver);
 				} else
 				{
-					results = tae.evaluateRun(runsToDo);
+				    tae.evaluateRunsAsync(runsToDo, taeCallback);
 				}
-					outputCompleted.set(true);
-				
-								
-				
+
+
+
+                if(!tae.areRunsPersisted() || mainOptions.waitForPersistedRunCompletion)
+                {
+                    log.debug("Waiting until validation completion");
+                    taeCallback.waitForCompletion();
+                } else
+                {
+
+                    // Presumably if the runs were done, then we already notified the callback.
+                    if ((tae.getNumberOfOutstandingBatches() > 1) && mainOptions.printStatus)
+                    {
+                        System.out.flush();
+                        System.out.println("Runs Submitted... Re-execute this utility to get the results.");
+                        System.out.flush();
+                    }
+                }
+
+                outputCompleted.set(true);
+
 			} 
 			
-			ObjectMapper map = new ObjectMapper();
-			JsonFactory factory = new JsonFactory();
-			factory.setCodec(map);
-			
-			
-			
-			ByteArrayOutputStream bout = new ByteArrayOutputStream();
-			JsonGenerator g = factory.createGenerator(bout);
 
-			SimpleModule sModule = new SimpleModule("MyModule", new Version(1, 0, 0, null));
-			map.configure(SerializationFeature.INDENT_OUTPUT, true);
-			  
-			map.registerModule(sModule);
-			
-			List<AlgorithmRunResult> results2 = new ArrayList<>(results);
-			g.writeObject(results2);
-			g.flush();
-			
-			System.out.flush();
-			System.out.println("******JSON******\n" + bout.toString("UTF-8") + "\n********");
-			System.out.flush();
-			
 		} catch(ParameterException e)
 		{	
 			log.error(e.getMessage());
@@ -183,101 +200,10 @@ public class JSONExecutor
 			{
 				log.error("Stack trace:",e);
 			}
+            System.exit(AEATKReturnValues.PARAMETER_EXCEPTION);
 		} catch(Exception e)
 		{
-			
-			e.printStackTrace();
+            System.exit(AEATKReturnValues.OTHER_EXCEPTION);
 		}
 	}
-	
-
-	
-	
-	/**
-	 * Encapsulated method for evaluating a run
-	 * 
-	 * @param runConfig 	runConfig to evaluate
-	 * @param tae 			target algorithm evaluator to use
-	 */
-	public static void processRunConfig(AlgorithmRunConfiguration runConfig, TargetAlgorithmEvaluator tae, final double killTime)
-	{
-		
-		
-		TargetAlgorithmEvaluatorRunObserver runStatus = new TargetAlgorithmEvaluatorRunObserver()
-		{
-			private long lastUpdate = 0;
-			@Override
-			public void currentStatus(List<? extends AlgorithmRunResult> runs) 
-			{
-				//As we print to standard out we want to make sure that a frequency that is too high doesn't spam the console
-				if(System.currentTimeMillis() - lastUpdate < 1000)
-				{
-					return;
-				}
-				
-				for(int i=0; i < runs.size(); i++)
-				{
-					AlgorithmRunResult run = runs.get(i);
-					//Log messages with more than 2 arguments, must use pass them as an array.
-					Object[] logArguments = { i, run.getAlgorithmRunConfiguration().getProblemInstanceSeedPair().getProblemInstance(), run.getRunStatus(), run.getRuntime()};
-					log.info("Run {} on {} has status =>  {}, {}", logArguments);
-					if(run.getRuntime() > killTime)
-					{
-						log.info("Dynamically killing run");
-						run.kill();
-					}
-				}
-				lastUpdate = System.currentTimeMillis();
-				
-			}
-		
-		};
-		
-		
-		//Invoke the runs with the observer
-		List<AlgorithmRunResult> runResults = tae.evaluateRun(Collections.singletonList(runConfig), runStatus); 
-		
-		 
-		log.info("Run Completed");
-		
-		for(int i=0; i < runResults.size(); i++)
-		{
-			//AlgorithmRun objects can be viewed as an "answer" to the RunConfig "question"
-			//They are IMMUTABLE.
-			AlgorithmRunResult run = runResults.get(i);
-		
-			//This is the same RunConfig as above
-			//But in general you should always use the information in the AlgorithmRun
-			AlgorithmRunConfiguration resultRunConfig = run.getAlgorithmRunConfiguration();
-
-			//Object representing whether the run reported SAT, UNSAT, TIMEOUT, etc...
-			RunStatus runResult = run.getRunStatus();
-		
-			double runtime = run.getRuntime();
-			double runLength = run.getRunLength();
-			double quality = run.getQuality();
-			
-			//The algorithm must echo back the seed that we request to it (historically this has helped with debugging)
-			long resultSeed = run.getResultSeed();
-			long requestSeed = resultRunConfig.getProblemInstanceSeedPair().getSeed();
-			
-			//Additional run data is just a string that the algorithm returned and we will keep track of.
-			String additionalData = run.getAdditionalRunData();
-			
-			if(resultSeed != requestSeed)
-			{ 
-				//A more complicated SLF4J log message. The {} are replaced with the parameters in order
-				log.error("Algorithm Run Result does not have a matching seed, requested: {} , returned: {}", resultSeed, requestSeed );
-			}
-			
-			//The toString() method does not return the actual configuration, this method is the best way to print them
-			String configString = resultRunConfig.getParameterConfiguration().getFormattedParameterString(ParameterStringFormat.NODB_OR_STATEFILE_SYNTAX);
-			
-			//Log messages with more than 2 parameters must have them passed as an array.
-			Object[] logArguments = { i, resultRunConfig.getProblemInstanceSeedPair().getProblemInstance(), configString, runResult, runtime, runLength, quality, resultSeed, additionalData};
-			log.info("Run {} on {} with config: {} had the result => {}, {}, {}, {}, {}, {}", logArguments);
-		}
-	}
-
-			
 }
--- src/ca/ubc/cs/beta/aeatk/example/jsonexecutor/JSONExecutorOptions.java
+++ src/ca/ubc/cs/beta/aeatk/example/jsonexecutor/JSONExecutorOptions.java
@@ -1,13 +1,12 @@
 package ca.ubc.cs.beta.aeatk.example.jsonexecutor;
 
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParametersDelegate;
-
 import ca.ubc.cs.beta.aeatk.help.HelpOptions;
 import ca.ubc.cs.beta.aeatk.logging.ConsoleOnlyLoggingOptions;
 import ca.ubc.cs.beta.aeatk.misc.options.UsageTextField;
 import ca.ubc.cs.beta.aeatk.options.AbstractOptions;
 import ca.ubc.cs.beta.aeatk.targetalgorithmevaluator.TargetAlgorithmEvaluatorOptions;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParametersDelegate;
 
 @UsageTextField(title="JSON Executor", description="Utility that reads a set of runs from standard in, in JSON format, and outputs the results in JSON when done")
 public class JSONExecutorOptions extends AbstractOptions{
@@ -23,6 +22,10 @@ public class JSONExecutorOptions extends AbstractOptions{
 
 	@Parameter(names="--print-status", description="If true, then periodically the status of outstanding runs will be logged")
 	public boolean printStatus = true;
+
+	@Parameter(names={"--wait-for-persistent-run-completion","--waitForPersistedRunCompletion"}, description="If the Target Algorithm Evaluator is persistent, then you can optionally not wait for it to finish, and come back later")
+	public boolean waitForPersistedRunCompletion = true;
+
 	
 	
 }
